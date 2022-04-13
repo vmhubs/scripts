e6c915c26498af1554a1e0cd5457ed4ceed22069
David.Hu <39669572+vmhubs@users.noreply.github.com>
2022-04-13 14:42:20 +0800

    Java environment variation Settings

---
+++ java.sh
@@ -0,0 +1,6 @@
+# Java environment variation Settings
+# Describe the position: /etc/profile.d/java.sh 
+export JAVA_HOME=/usr/java/jdk1.8.0_321
+export PATH=$JAVA_HOME/bin:$PATH
+export CLASSPATH=.:$JAVA_HOME/lib/dt.jar:$JAVA_HOME/lib/tools.jar
+export JAVA_PATH=$JAVA_HOME/bin:$JRE_HOME/bin
